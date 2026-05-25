c772eecf9d4fbd4067a520c76f43bce7f720b7cb
Murphy <54226881+murphys7017@users.noreply.github.com>
2026-05-26 02:29:18 +0800

    refactor: introduce pet desktop runtime root

--- frontend/src/views/PetDesktopView.vue
+++ frontend/src/views/PetDesktopView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import DesktopPetCanvas from "../components/DesktopPetCanvas.vue";
 import { usePetDesktopController } from "../app/usePetDesktopController";
+import { providePetDesktopRuntime } from "../app/usePetDesktopRuntime";
 
+providePetDesktopRuntime();
 const { selectedModel, stageMessage, showContextMenu } = usePetDesktopController();
 </script>
 
